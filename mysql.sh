7220eb773eb8d657b9d42ccd073e618439161d0d
s poorna chandra <poornanaidu1@gmail.com>
2023-08-26 20:27:12 +0530

    roboshop scripts update& DNS records

---
+++ mysql.sh
@@ -0,0 +1,6 @@
+cp mysql /etc/systemd/system/mysql.repo
+yum module disable mysql -y
+yum install mysql-community-server -y
+mysql_secure_installation --set-root-pass RoboShop@1
+systemctl enable mysqld
+systemctl restart mysqld
