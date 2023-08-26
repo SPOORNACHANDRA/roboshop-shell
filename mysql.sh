49b342c2ca01e945d5f9cab39c49efa89172b95f
s poorna chandra <poornanaidu1@gmail.com>
2023-08-26 22:06:47 +0530

    roboshop scripts update& DNS records

--- mysql.sh
+++ mysql.sh
@@ -1,4 +1,4 @@
-cp mysql /etc/systemd/system/mysql.repo
+cp mysql.repo /etc/systemd/system/mysql.repo
 yum module disable mysql -y
 yum install mysql-community-server -y
 mysql_secure_installation --set-root-pass RoboShop@1
