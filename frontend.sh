2d1b2fedb7658b21c5e124d1b2d801309f160531
s poorna chandra <poornanaidu1@gmail.com>
2023-08-27 14:28:07 +0530

    roboshop scripts update& DNS records

--- frontend.sh
+++ frontend.sh
@@ -1,8 +1,6 @@
 cp roboshop.conf /etc/nginx/default.d/roboshop.conf
 
 yum install nginx -y
-systemctl enable nginx
-systemctl start nginx
 
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
