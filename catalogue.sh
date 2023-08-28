e8372d8e4871f81cb79010274af76816e57df662
s poorna chandra <poornanaidu1@gmail.com>
2023-08-28 19:13:36 +0530

    echo command in project script

--- catalogue.sh
+++ catalogue.sh
@@ -1,28 +1,28 @@
-echo -e " \e[31m >>>>>>>>>> create catalogue service <<<<<<<<<>[\e[om"
+echo -e " \e[31m>>>>>>>>>> create catalogue service <<<<<<<<<\e[0m"
 cp catalogue.service /etc/systemd/system/catalogue.service
-echo -e " \e[31m>>>>>>>>>> create mongo repo <<<<<<<<<>[\e[om"
+echo -e " \e[31m>>>>>>>>>> create mongo repo <<<<<<<<<\e[0m"
 cp mongo.repo /etc/yum.repos.d/mongo.repo
-echo -e " \e[31m>>>>>>>>>> create nodejs repos <<<<<<<<<>[\e[om"
+echo -e " \e[31m>>>>>>>>>> create nodejs repos <<<<<<<<<\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-echo -e " \e[31m>>>>>>>>>> install nodejs  <<<<<<<<<>[\e[om"
+echo -e " \e[31m>>>>>>>>>> install nodejs  <<<<<<<<<\e[0m"
 yum install nodejs -y
-echo -e " \e[31m>>>>>>>>>> create an application user <<<<<<<<<>[\e[om"
+echo ">>>>>>>>>> create an application user <<<<<<<<<\e[0m"
 useradd roboshop
-echo -e " \e[31m>>>>>>>>>> create app directory <<<<<<<<>[\e[om"
+echo -e " \e[31m>>>>>>>>>> create app directory <<<<<<<<\e[0m"
 mkdir /app
-echo -e " \e[31m>>>>>>>>>> download app content  <<<<<<<<<>[\e[om"
+echo -e " \e[31m>>>>>>>>>> download app content  <<<<<<<<<\e[0m"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
-echo -e " \e[31m>>>>>>>>>> app extraction  <<<<<<<<<>[\e[om"
+echo -e " \e[31m>>>>>>>>> app extraction  <<<<<<<<<\e[0m"
 cd /app
 unzip /tmp/catalogue.zip
 cd /app
-echo -e " \e[31m>>>>>>>>>> download Nodejs dependencies <<<<<<<<<>[\e[om"
+echo -e " \e[31m>>>>>>>>> download Nodejs dependencies <<<<<<<<<\e[0m"
 npm install
-echo -e " \e[31m>>>>>>>>>> install mongodb client <<<<<<<<<>[\e[om"
+echo -e " \e[31m>>>>>>>>>> install mongodb client <<<<<<<<<\e[0m"
 yum install mongodb-org-shell -y
-echo -e " \e[31m>>>>>>>>>> load catalogue schema <<<<<<<<<>[\e[om"
+echo -e " \e[31m>>>>>>>>>> load catalogue schema <<<<<<<<<\e[0m"
 mongo --host mongodb.poornadevops.online </app/schema/catalogue.js
-echo -e " \e[31m>>>>>>>>>> create catalogue service <<<<<<<<<>[\e[om"
+echo -e " \e[31m>>>>>>>>>> create catalogue service <<<<<<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl start catalogue
