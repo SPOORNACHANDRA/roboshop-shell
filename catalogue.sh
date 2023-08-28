8074eb2b6cd2741ca97f9022c6985915b4707bc6
s poorna chandra <poornanaidu1@gmail.com>
2023-08-28 18:51:48 +0530

    echo command in project script

--- catalogue.sh
+++ catalogue.sh
@@ -1,17 +1,28 @@
+echo ">>>>>>>>>> create catalogue service <<<<<<<<<<"
 cp catalogue.service /etc/systemd/system/catalogue.service
+echo ">>>>>>>>>> create mongo repo <<<<<<<<<<"
 cp mongo.repo /etc/yum.repos.d/mongo.repo
+echo ">>>>>>>>>> create nodejs repos <<<<<<<<<<"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+echo ">>>>>>>>>> install nodejs  <<<<<<<<<<"
 yum install nodejs -y
+echo ">>>>>>>>>> create an application user <<<<<<<<<<"
 useradd roboshop
+echo ">>>>>>>>>> create app directory <<<<<<<<<"
 mkdir /app
+echo ">>>>>>>>>> download app content  <<<<<<<<<<"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+echo ">>>>>>>>>> app extraction  <<<<<<<<<<"
 cd /app
 unzip /tmp/catalogue.zip
 cd /app
+echo ">>>>>>>>>> download Nodejs dependencies <<<<<<<<<<"
 npm install
+echo ">>>>>>>>>> install mongodb client <<<<<<<<<<"
+yum install mongodb-org-shell -y
+echo ">>>>>>>>>> load catalogue schema <<<<<<<<<<"
+mongo --host mongodb.poornadevops.online </app/schema/catalogue.js
+echo ">>>>>>>>>> create catalogue service <<<<<<<<<<"
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl start catalogue
-yum install mongodb-org-shell -y
-mongo --host mongodb.poornadevops.online </app/schema/catalogue.js
-systemctl restart catalogue
