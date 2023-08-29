8b846061a40088db38fdbee96ae53125cda6d9f9
s poorna chandra <poornanaidu1@gmail.com>
2023-08-29 17:21:58 +0530

    schema setup in roboshop

--- common.sh
+++ common.sh
@@ -8,7 +8,7 @@ func_systemd() {
 
 
 func_schema_setup() {
-  if ["$schema_type" =="mongodb" ]; then
+  if [ "$schema_type" == "mongodb" ]; then
    echo -e " \e[31m>>>>>>>>>> install mongodb client <<<<<<<<<\e[0m"
     yum install mongodb-org-shell -y &>>${log}
     echo -e " \e[31m>>>>>>>>>> load ${component} schema <<<<<<<<<\e[0m"
