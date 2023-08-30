b2c4c27faf1f2b09638db5dca3f76fa4bf7f9a0f
s poorna chandra <poornanaidu1@gmail.com>
2023-08-30 15:48:09 +0530

    practice script

--- common.sh
+++ common.sh
@@ -100,6 +100,7 @@ func_python() {
  func_exit_status
   func_apppreq
   sed -i "s/rabbitmq_app_password/${rabbitmq_app_password}/" /etc/systemd/system/${component}.service
+
   echo -e " \e[31m>>>>>>>>>> build ${component}  <<<<<<<<<\e[0m"
   pip3.6 install -r requirements.txt &>>${log}
   func_exit_status
